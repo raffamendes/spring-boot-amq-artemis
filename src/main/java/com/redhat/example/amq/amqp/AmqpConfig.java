7913b097f219dc7094bc321b25f022e7a9b13fd6
Raffael Mendes <raffael.pikachu@gmail.com>
2021-09-20 16:07:32 -0300

    Adding message info

--- src/main/java/com/redhat/example/amq/amqp/AmqpConfig.java
+++ src/main/java/com/redhat/example/amq/amqp/AmqpConfig.java
@@ -10,55 +10,62 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.qpid.jms.JmsConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.redhat.example.amq.core.Consumer;
+
 @Component
 public class AmqpConfig {
-	
+
 	@Value("${amqp.broker-url}")
 	private String brokerUrl;
-	
+
 	@Value("${amqp.queue.name}")
 	private String queueName;
 	
-	
+	private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+
 	public void sender(String message) throws JMSException {
-		 Connection connection = null;
-	      ConnectionFactory connectionFactory = new JmsConnectionFactory(brokerUrl);
+		Connection connection = null;
+		logger.info("camqp connection uri={}",brokerUrl);
+		ConnectionFactory connectionFactory = new JmsConnectionFactory(brokerUrl);
 
-	      try {
+		try {
 
-	         // Step 1. Create an amqp qpid 1.0 connection
-	         connection = connectionFactory.createConnection();
+			// Step 1. Create an amqp qpid 1.0 connection
+			connection = connectionFactory.createConnection();
 
-	         // Step 2. Create a session
-	         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			// Step 2. Create a session
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-	         // Step 3. Create a sender
-	         Queue queue = session.createQueue(queueName);
-	         MessageProducer sender = session.createProducer(queue);
+			// Step 3. Create a sender
+			Queue queue = session.createQueue(queueName);
+			MessageProducer sender = session.createProducer(queue);
 
-	         // Step 4. send a few simple message
-	         sender.send(session.createTextMessage(message));
+			// Step 4. send a few simple message
+			sender.send(session.createTextMessage(message));
 
-	         connection.start();
+			connection.start();
 
-	         // Step 5. create a moving receiver, this means the message will be removed from the queue
-	         MessageConsumer consumer = session.createConsumer(queue);
+			// Step 5. create a moving receiver, this means the message will be removed from the queue
+			MessageConsumer consumer = session.createConsumer(queue);
 
-	         // Step 7. receive the simple message
-	         TextMessage m = (TextMessage) consumer.receive(5000);
-	         System.out.println("message = " + m.getText());
+			// Step 7. receive the simple message
+			TextMessage m = (TextMessage) consumer.receive(5000);
+			System.out.println("message = " + m.getText());
 
-	      } catch (JMSException e) {
+		} catch (JMSException e) {
 			e.printStackTrace();
 		} finally {
-	         if (connection != null) {
-	            // Step 9. close the connection
-	            connection.close();
-	         }
-	      }
+			if (connection != null) {
+				// Step 9. close the connection
+				connection.close();
+			}
+		}
 
 	}
 
